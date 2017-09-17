7a4cfbbf771f26a31b8c4e0ac75a579007b8c02d
CHBaker <charlesprobaker@mail.com>
2017-09-17 13:33:06 -0600

    REF+PERF+CHORE: organize js code for http
    REF: UPDATE js code for http requests, wrap init with jQuery function
    
    PERF: code is no longer called in-line, REMOVE unnecessary scripts from
    html
    
    CHORE: DELETE config.js, useless for now

--- js/restat.js
+++ js/restat.js
@@ -1,98 +1,118 @@
 
-// CPU Initial
-$.get("http://d8cc.dspi.org/rest/cpu/test", function(result){
-	$.each(result, function(i, field){
-		console.log(field);
-		$('.cpu').text(field.cpudata + "%");
-	});
-});
+$(document).ready( function() {
 
-// CPU Interval
-setInterval(function() {
-    // method to be executed;
-    $.get("http://d8cc.dspi.org/rest/cpu/test", function(result){
+    function getCpu() {
+        $.get("http://d8cc.dspi.org/rest/cpu/test", (response) => {
+            $.each(response, function(i, field) {
+                console.log(field)
+                $('.cpu').text(field.cpudata + "%")
+            })
+        })
+    }
+
+    function setCpuInterval() {
+        setInterval(function() {
+            $.get("http://d8cc.dspi.org/rest/cpu/test", (response) => {
+            	$.each(response, function(i, field) {
+            		console.log(field);
+            		$('.cpu').text(field.cpudata + "%");
+            	});
+            });
+        }, 11000);
+    }
+
+    function getUptime() {
+        $.get("http://d8cc.dspi.org/rest/uptimestr/test", (response) => {
+    	   $.each(response, function(i, field) {
+        		console.log(field);
+        		$('.uptime').text(field.uptimedata);
+        	});
+        });
+    }
+
+    function setUptimeInterval() {
+        setInterval(function() {
+        // method to be executed;
+        $.get("http://d8cc.dspi.org/rest/uptimestr/test", (response) => {
+        	$.each(response, function(i, field) {
+        		console.log(field);
+        		$('.uptime').text(field.uptimedata);
+            	});
+            });
+        }, 2000);
+    }
+
+    function getMem() {
+        $.get("http://d8cc.dspi.org/rest/mem/test", (response) => {
+        	$.each(response, function(i, field) {
+        		console.log(field);
+        		$('.memory').append(field.memdata);
+        	});
+        });
+    }
+
+    function setMemInterval() {
+        setInterval(function() {
+            $.get("http://d8cc.dspi.org/rest/mem/test", (response) => {
+            	$.each(response, function(i, field) {
+            		console.log(field);
+            		$('.memory').text(field.memdata);
+            	});
+            });
+        }, 5000);
+    }
+
+    function getNet() {
+        $.get("http://d8cc.dspi.org/rest/netstr/test", (response) => {
+        	$.each(response, function(i, field) {
+        		console.log(field);
+        		$('.net').append(field.netstrdata);
+        	});
+        });
+    }
+
+    function setNetInterval() {
+        setInterval(function() {
+            $.get("http://d8cc.dspi.org/rest/netstr/test", (response) => {
+            	$.each(response, function(i, field) {
+            		console.log(field);
+            		$('.net').text(field.netstrdata);
+            	});
+            });
+        }, 15000);
+    }
+
+    /*
+
+    // Uptime 
+    $.get("http://d8cc.dspi.org/rest/uptime/test", function(result){
     	$.each(result, function(i, field){
     		console.log(field);
-    		$('.cpu').text(field.cpudata + "%");
+    		$('.uptime').append('Uptime: ' +field.uptimesec + " " + field.uptimestr + "<br />");
     	});
     });
-}, 11000);
 
-// Uptime Initial
-$.get("http://d8cc.dspi.org/rest/uptimestr/test", function(result){
-	$.each(result, function(i, field){
-		console.log(field);
-		$('.uptime').text(field.uptimedata);
-	});
-});
-
-// Uptime Interval
-setInterval(function() {
-    // method to be executed;
-    $.get("http://d8cc.dspi.org/rest/uptimestr/test", function(result){
-    	$.each(result, function(i, field){
-    		console.log(field);
-    		$('.uptime').text(field.uptimedata);
-    	});
-    });
-}, 2000);
-
-// Mem Initial
-$.get("http://d8cc.dspi.org/rest/mem/test", function(result){
-	$.each(result, function(i, field){
-		console.log(field);
-		$('.memory').append(field.memdata);
-	});
-});
-
-// Mem Interval
-setInterval(function() {
-    // method to be executed;
+    // Mem 
     $.get("http://d8cc.dspi.org/rest/mem/test", function(result){
-    	$.each(result, function(i, field){
-    		console.log(field);
-    		$('.memory').text(field.memdata);
+        var memoryJoin = [];
+        $.each(result, function(i, field){
+        if (field.totalmem !== undefined) $('.mem').append('Memory (Total): ' + field.totalmem + "<br />");
+        if (field.freemem !== undefined) $('.mem').append('Memory (Free): ' + field.freemem + "<br />");
+        if (field.loadmemp !== undefined) $('.mem').append('Memory (Used): ' + field.loadmemp + "<br />");
     	});
     });
-}, 5000);
 
-// Net Initial
-$.get("http://d8cc.dspi.org/rest/netstr/test", function(result){
-	$.each(result, function(i, field){
-		console.log(field);
-		$('.net').append(field.netstrdata);
-	});
+    */
+    function Init() {
+        getCpu()
+        setCpuInterval()
+        getUptime()
+        setUptimeInterval()
+        getMem()
+        setMemInterval()
+        getNet()
+        setNetInterval()
+    }
+
+    Init()
 });
-
-// Net Interval
-setInterval(function() {
-    // method to be executed;
-    $.get("http://d8cc.dspi.org/rest/netstr/test", function(result){
-    	$.each(result, function(i, field){
-    		console.log(field);
-    		$('.net').text(field.netstrdata);
-    	});
-    });
-}, 15000);
-
-/*
-
-// Uptime 
-$.get("http://d8cc.dspi.org/rest/uptime/test", function(result){
-	$.each(result, function(i, field){
-		console.log(field);
-		$('.uptime').append('Uptime: ' +field.uptimesec + " " + field.uptimestr + "<br />");
-	});
-});
-
-// Mem 
-$.get("http://d8cc.dspi.org/rest/mem/test", function(result){
-    var memoryJoin = [];
-    $.each(result, function(i, field){
-    if (field.totalmem !== undefined) $('.mem').append('Memory (Total): ' + field.totalmem + "<br />");
-    if (field.freemem !== undefined) $('.mem').append('Memory (Free): ' + field.freemem + "<br />");
-    if (field.loadmemp !== undefined) $('.mem').append('Memory (Used): ' + field.loadmemp + "<br />");
-	});
-});
-
-*/
